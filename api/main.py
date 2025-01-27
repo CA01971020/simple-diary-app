fbf6015608563a0fbd0dbb344d33819a7124d3d7
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-01-27 12:14:30 +0900

    create関数のエンドポイントの追加

--- api/main.py
+++ api/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
-from diary_data import datas as diarys_date
+from fastapi import FastAPI,Body
+from diary_data import datas as diarys_data
 
 app = FastAPI()
 
 @app.get("/datas")
 async def diarys_all():
-    return diarys_date.diarys_all()
+    return diarys_data.diarys_all()
+
+@app.post("/datas")
+async def create(diary_create=Body()):
+    return diarys_data.create(diary_create)
