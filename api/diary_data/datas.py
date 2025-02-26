337b218d6438cbb3e92fbf7c65847194a731fe45
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-02-27 02:07:39 +0900

    Update datas.py

--- api/diary_data/datas.py
+++ api/diary_data/datas.py
@@ -1,15 +1,17 @@
+from api.schemas import DiaryCreate
+
 class Diary:
     def __init__(
         self,
         id:int,
         title:str,
         diary_data:str,
-        diary:str,
+        diary_text:str,
     ):
         self.id = id
         self.title =title
         self.diary_data = diary_data
-        self.diary = diary
+        self.diary_text = diary_text
 
 diarys = [
     Diary(1,"テスト1","2025年1月27日","テストデータだよ1"),
@@ -20,12 +22,12 @@ diarys = [
 def diarys_all():
     return diarys
 
-def create(diary_create):
+def create(diary_create:DiaryCreate):
     new_diary = Diary(
         len(diarys) + 1,
-        diary_create.get("title"),
-        diary_create.get("diary_data"),
-        diary_create.get("diary"),
+        diary_create.title,
+        diary_create.diary_data,
+        diary_create.diary_text
     )
     diarys.append(new_diary)
     return(new_diary)
