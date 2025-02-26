53d669f83e151c424166dc7bbebb153e111f6b43
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-02-27 02:38:32 +0900

    endpoint設定

--- api/routers/diary.py
+++ api/routers/diary.py
@@ -5,10 +5,10 @@ from schemas import DiaryCreate
 
 router = APIRouter(prefix="/diarys",tags=["Diarys"])
 
-@router.get("",status_code=status.HTTP_200_OK)
+@router.get("/",status_code=status.HTTP_200_OK)
 async def diarys_all():
     return diarys_data.diarys_all()
 
-@router.post("",status_code=status.HTTP_201_CREATED)
+@router.post("/",status_code=status.HTTP_201_CREATED)
 async def create(diary_create:DiaryCreate):
     return diarys_data.create(diary_create)
